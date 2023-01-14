3fe8894d505d748a00a983984106993a4367a175
Hyun, Jaeyeon <93899740+jyhyun1008@users.noreply.github.com>
2023-01-15 07:32:23 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,15 @@
+let vh = window.innerHeight * 0.01;
+let vw = window.innerWidth * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+// 리사이즈
+window.addEventListener('resize', () => {
+    let vh = window.innerHeight * 0.01;
+    let vw = window.innerWidth * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+})
+
+
 function parseMd(md){
 
     var md0 = md;
